05b0d5e660a396ad5f5b169be506a36a52f4b0b6
Kosan3 <ichiro.neko.0421@gmail.com>
2020-05-06 14:39:31 +0900

    [Finish]5-4

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,4 +1,14 @@
 class PostCommentsController < ApplicationController
 	def create
+		post_image = PostImage.find(params[:post_image_id])
+		post_comment = current_user.post_comments.new(post_comment_params)
+		post_comment.post_image_id = post_image.id
+		post_comment.save
+		redirect_to post_image_path(post_image)
+	end
+
+	private
+	def post_comment_params
+		params.require(:post_comment).permit(:comment)
 	end
 end
